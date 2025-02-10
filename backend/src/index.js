8e08ede112697edf7692da4c1158c8ee6dfcc833
baz <88bazanr@gmail.com>
2025-02-10 08:28:52 -0300

    #### Socket.io Implementation (06:53:32)

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
-import dotenv from "dotenv";
 import express from "express";
+import dotenv from "dotenv";
 import { clerkMiddleware } from "@clerk/express";
 import fileUpload from "express-fileupload";
 import path from "path";
 import cors from "cors";
+import { createServer } from "http";
 
 
+import { initializeSocket } from "./lib/socket.js";
 import { connectDB } from "./lib/db.js";
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
@@ -19,6 +21,9 @@ dotenv.config();
 const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT;
+
+const httpServer = createServer(app);
+initializeSocket(httpServer);
 
 app.use(cors(
   {
@@ -52,7 +57,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal Server Error" : err.message });
 });
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
   connectDB();
 });
