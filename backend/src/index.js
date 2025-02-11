f02f9cb536612130f22924ec52545edfac91a213
baz <88bazanr@gmail.com>
2025-02-11 02:20:02 -0300

    deployando

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,10 +4,12 @@ import { clerkMiddleware } from "@clerk/express";
 import fileUpload from "express-fileupload";
 import path from "path";
 import cors from "cors";
+import fs from "fs";
 import { createServer } from "http";
-
+import cron from "node-cron";
 
 import { initializeSocket } from "./lib/socket.js";
+
 import { connectDB } from "./lib/db.js";
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
@@ -45,12 +47,36 @@ app.use(
   })
 );
 
+// cron jobs
+// delete those files in every 1 hour, or 1 minute, or every 00h
+const tempDir = path.join(process.cwd(),"tmp")
+cron.schedule("0 * * * *", () => {
+  if (fs.existsSync(tempDir)) {
+    fs.readdir(tempDir, (err, files) => {
+      if (err) {
+        console.log("error", err);
+        return;
+      }
+      for (const file of files) {
+        fs.unlink(path.join(tempDir, file), (err) => {});
+      }
+    })
+  }
+})
+
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
 
 // error handler
 app.use((err, req, res, next) => {
